e5630fd38c314248f3b5e990469d218b0bca94c9
Bamatembera <Christian.Bamatembera@cyber.gc.ca>
2021-01-08 12:08:40 -0500

    fix typo

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.time.Instant;
 
 @SpringBootApplication
-public class HelloworldrApplication {
+public class HelloworldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloworldrApplication.class, args);
+		SpringApplication.run(HelloworldApplication.class, args);
 		String asciiArt = FigletFont.convertOneLine("Hello k8s");
 		System.out.println(asciiArt);
 		System.out.println("Yay Me!");
