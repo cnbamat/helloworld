dcc1d0263c81ec2515d7df99d3b8ff350ce31182
Bamatembera <Christian.Bamatembera@cyber.gc.ca>
2021-01-08 11:38:41 -0500

    Testing cron workload

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -1,14 +1,20 @@
-package com.example.helloworld;
+package com.github.palyfight.hellodocker;
 
+import com.github.lalyos.jfiglet.FigletFont;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Instant;
+
 @SpringBootApplication
-public class HelloworldApplication {
+public class HellodockerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloworldApplication.class, args);
-		System.out.println("Hello World");
+		SpringApplication.run(HellodockerApplication.class, args);
+		String asciiArt = FigletFont.convertOneLine("Hello k8s");
+		System.out.println(asciiArt);
+		System.out.println("Yay Me!");
+		System.out.println(Instant.now());
 	}
 
 }
